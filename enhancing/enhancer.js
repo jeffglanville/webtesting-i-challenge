4ff76165cf9652af87d87a56ef535c3b9540eaf9
Jeff Glanville <jeff.glanville@outlook.com>
2020-09-11 21:36:49 -0500

    update

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,23 @@ module.exports = {
 };
 
 function success(item) {
-  return { ...item };
+  if (item.enhancement === 20) {
+    return { ...item };
+  }
+  return{...item, enhancement: item.enhancement + 1};
 }
 
 function fail(item) {
-  return { ...item };
+  if(item.enhancement < 15) {
+    return {...item, durability: item.durability - 5}
+  }else if (item.enhancement >= 15 && item.enhancement < 17) {
+    return{...item, durability: item.durability - 10}
+  }
+  return { ...item, durability: item.durability - 1 };
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item, durability: 100 };
 }
 
 function get(item) {
